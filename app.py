0fa94a3fadab95523274f4a55bd382debee037c3
isasasa-hue <isabella.whitsny@gmail.com>
2025-07-18 01:33:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -365,5 +365,5 @@ def historial():
     '''
     return page_template("Historial de Transferencias", tabla)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=True)
